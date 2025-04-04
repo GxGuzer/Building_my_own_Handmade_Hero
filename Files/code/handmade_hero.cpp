3844920e4259208a1833731ba30e80874af68fa6
GxGuzer <xgustavo610@gmail.com>
2025-04-04 18:30:04 -0300

    i forgot to save before pushing the last time

--- Files/code/handmade_hero.cpp
+++ Files/code/handmade_hero.cpp
@@ -1,12 +1,7 @@
 #include <Windows.h>
-#include <iostream>
-using namespace std;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
   PSTR lpCmdLine, int nCmdShow)
 {
-  AllocConsole();
-  cout << "Hello World!";
-  cin.get();
   return 0;
 }

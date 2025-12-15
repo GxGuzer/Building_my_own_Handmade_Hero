c63f3786fd0f699ece4b9bfd41dfa8d430036fc2
GxGuzer <xgustavo610@gmail.com>
2025-12-15 16:33:34 -0300

    a

--- Files/build/debug.bat
+++ Files/build/debug.bat
@@ -1,3 +1,4 @@
 @echo off
 
 start devenv "C:/Users/xgust/OneDrive/Documentos/GitHub/Building_my_own_Handmade_Hero/Files/build/handmade_hero.exe"
+echo Opening Microsoft Visual Studio 2022...
--- Files/build/desktop.bat
+++ Files/build/desktop.bat
@@ -1,3 +1,4 @@
 @echo off
 
 call "C:\Users\xgust\AppData\Local\GitHubDesktop\GitHubDesktop.exe" 
+echo Opening GitHub Desktop...
